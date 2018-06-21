a68d18de3e8312abc9de6aa2a08fa272f930261e
AustynT <Austyntravis9@gmail.com>
2018-06-21 10:31:22 -0600

    added clock component

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import Clock from './Clock'
 
 
 class BirthdayForm extends Component {
@@ -25,7 +26,6 @@ class BirthdayForm extends Component {
     }
 
     handleGenerate(){
-        alert('trying to generate')
         this.setState({
             formCompleted: true
         })
@@ -36,14 +36,24 @@ class BirthdayForm extends Component {
     render(){
         return( 
         <div>
-            <h1> Birthday Form Component </h1>
-            <DatePicker 
+            {
+                this.state.formCompleted ? 
+                <div>
+                    <Clock birthdayFormState={this.state}  />
+                </div>
+                :
+                <div>
+                <h1> Birthday Form Component </h1>
+                <DatePicker 
                 selected={this.state.startDate}
                 onChange={this.handleChange}
-            />
+                />
             <a onClick={this.handleGenerate}>Generate Count Down </a>
+                </div>
+            }
         </div>
-        )}
+        )
+    }
 
 }
 
